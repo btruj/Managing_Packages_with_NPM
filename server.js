9f4807d00dbe355c6541df41b615344f22b059ff
Your Name <Your Email>
2022-10-31 21:00:45 -0400

    LOTS of changes

--- server.js
+++ server.js
@@ -3,9 +3,9 @@
  * the verification process may break
  * ***************************************************/
 
-//'use strict';
+// 'use strict';
 
-//var fs = require('fs');
+// var fs = require('fs');
 var bGround = require('fcc-express-bground');
 var myApp = require('./myApp');
 var express = require('express');
@@ -24,39 +24,38 @@ if (!process.env.DISABLE_XORIGIN) {
   });
 }
 
-/*app.use('/public', express.static(process.cwd() + '/public'));
+// app.use('/public', express.static(process.cwd() + '/public'));
+//
+// app.route('/_api/package.json')
+//   .get(function(req, res, next) {
+//     console.log('requested');
+//     fs.readFile(__dirname + '/package.json', function(err, data) {
+//       if(err) return next(err);
+//       res.type('txt').send(data.toString());
+//     });
+//   });
+//
+// app.route('/')
+//     .get(function(req, res) {
+// 		  res.sendFile(process.cwd() + '/views/index.html');
+//     })
+//
+// // Respond not found to all the wrong routes
+// app.use(function(req, res, next){
+//   res.status(404);
+//   res.type('txt').send('Not found');
+// });
+//
+// // Error Middleware
+// app.use(function(err, req, res, next) {
+//   if(err) {
+//     res.status(err.status || 500)
+//       .type('txt')
+//       .send(err.message || 'SERVER ERROR');
+//   }
+// })
 
-app.route('/_api/package.json')
-  .get(function(req, res, next) {
-    console.log('requested');
-    fs.readFile(__dirname + '/package.json', function(err, data) {
-      if(err) return next(err);
-      res.type('txt').send(data.toString());
-    });
-  });
-
-app.route('/')
-    .get(function(req, res) {
-		  res.sendFile(process.cwd() + '/views/index.html');
-    })
-
-// Respond not found to all the wrong routes
-app.use(function(req, res, next){
-  res.status(404);
-  res.type('txt').send('Not found');
-});
-
-// Error Middleware
-app.use(function(err, req, res, next) {
-  if(err) {
-    res.status(err.status || 500)
-      .type('txt')
-      .send(err.message || 'SERVER ERROR');
-  }
-})*/
-
-//Listen on port set in environment variable or default to 3000
 var port = process.env.PORT || 3000;
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function () {
-  bGround.log("Node.js listening on port" + port + '...');
+  bGround.log('Node.js listening on port' + port + "...");
 });
